c8c553c95685df7d5d2f52882220f173c30fd7ec
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2015-11-13 22:59:11 -0500

    added more npm commands

--- npm-completion.sh
+++ npm-completion.sh
@@ -1,9 +1,9 @@
 _npm_completion () {
   CUR=${COMP_WORDS[COMP_CWORD]}
   FIRST_LETTER=$(echo $CUR | head -c 1)
-  if [ $3 = "install" ] && [ ! -z $FIRST_LETTER ]; then
+  if [ $3 = "install" -o $3 = "i" ] && [ ! -z $FIRST_LETTER ]; then
     COMPREPLY=( $( compgen -W "$(cat $PATH_TO_NPM_COMPLETION/keys/npm-$FIRST_LETTER)" -- $CUR ) )
-  elif [ $3 = "remove" ]; then
+  elif [ $3 = "update" -o $3 = "remove" -o $3 = "rm" -o $3 = "r" -o $3 = "un" -o $3 = "unlink" -o $3 = "uninstall" ]; then
     COMPREPLY=( $( compgen -W "$(ls ~/.npm 2>/dev/null)" -- $CUR ) )
   else
     local si="$IFS"
