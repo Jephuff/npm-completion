919bb979f6360513c68e0677f256228fe0f90cf8
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2015-11-18 20:47:34 -0500

    implemented update and remove commands

--- npm-completion.sh
+++ npm-completion.sh
@@ -1,16 +1,49 @@
 _npm_completion () {
   CUR=${COMP_WORDS[COMP_CWORD]}
-  if [ $3 = "install" -o $3 = "i" ]; then
+  echo "${COMP_WORDS[@]}" | grep ' -g\([^a-zA-Z0-9]\|$\)' > /dev/null 2>/dev/null
+  GLOBAL=$?
+  IDX=$(expr $COMP_CWORD - 1)
+  CMD=${COMP_WORDS[IDX]}
+  while [ "${CMD:0:1}" == "-"  ]; do
+    IDX=$(expr $IDX - 1)
+    CMD=${COMP_WORDS[IDX]}
+  done
+
+
+  NPM_BIN=$(readlink -f $(which npm) | grep -o '^.*node_modules')
+
+  DO_DEFAULT=true
+  if [ $CMD = "install" -o $CMD = "i" ]; then
+    DO_DEFAULT=false
     COMPREPLY=( $( grep "^$CUR" "$PATH_TO_NPM_COMPLETION/keys/npm-all" ) )
-  elif [ $3 = "update" -o $3 = "remove" -o $3 = "rm" -o $3 = "r" -o $3 = "un" -o $3 = "unlink" -o $3 = "uninstall" ]; then
-    COMPREPLY=( $( compgen -W "$(ls ~/.npm 2>/dev/null)" -- $CUR ) )
-  else
+  elif [ $CMD = "update" -o $CMD = "remove" -o $CMD = "rm" -o $CMD = "r" -o $CMD = "un" -o $CMD = "unlink" -o $CMD = "uninstall" ]; then
+    if [ $GLOBAL = 0 ]; then
+      if [ ! -z $NPM_BIN ]; then
+        DO_DEFAULT=false
+        pushd $NPM_BIN >/dev/null
+        COMPREPLY=( $( compgen -d -- $CUR ) )
+        popd >/dev/null
+      fi
+    else
+      DO_DEFAULT=false
+      DIR=$(pwd)
+
+      while [ ! -d node_modules -a $(pwd) != "/" ]; do
+        cd ../
+      done
+
+      if [ -d node_modules ]; then
+        cd node_modules
+        COMPREPLY=( $( compgen -d -- $CUR ) )
+      fi
+
+      cd $DIR
+    fi
+  fi
+
+  if [ $DO_DEFAULT = true ]; then
     local si="$IFS"
-    IFS=$'\n' COMPREPLY=($(COMP_CWORD="$COMP_CWORD" \
-                           COMP_LINE="$COMP_LINE" \
-                           COMP_POINT="$COMP_POINT" \
-                           npm completion -- "${COMP_WORDS[@]}" \
-                           2>/dev/null)) || return $?
+    IFS=$'\n' COMPREPLY=($(COMP_CWORD="$COMP_CWORD" COMP_LINE="$COMP_LINE" COMP_POINT="$COMP_POINT" npm completion -- "${COMP_WORDS[@]}" 2>/dev/null)) || return $?
     IFS="$si"
   fi
 }
