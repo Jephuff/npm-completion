c4a1549f35e76e3e2855a2cac0f196576ee2cacd
Jeffrey Burt <jeffrey.burt@karmacasting.com>
2019-02-11 22:18:14 -0500

    fix uninstall

--- get-source-files.sh
+++ get-source-files.sh
@@ -6,16 +6,13 @@ if [ $user = "root" ]; then
   user=$( who am i 2> /dev/null | awk '{print $1}')
 fi
 USER_HOME=$(eval echo ~$user)
-
 # check for source files
 if [ -e $USER_HOME/.bashrc ]; then
-  BASH_SOURCE_FILE="$USER_HOME/.bashrc"
+  echo "$USER_HOME/.bashrc"
 elif [ -e $USER_HOME/.bash_profile ]; then
-  BASH_SOURCE_FILE="$USER_HOME/.bash_profile"
+  echo "$USER_HOME/.bash_profile"
 fi
 
 if [ -e $USER_HOME/.zshrc ]; then
-  ZSH_SOURCE_FILE="$USER_HOME/.zshrc"
+  echo "$USER_HOME/.zshrc"
 fi
-
-echo "$ZSH_SOURCE_FILE $BASH_SOURCE_FILE"
